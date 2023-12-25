010119a88a4a4ef2ebdaa00935ff9aac9dc98f07
khghouse <khghouse@naver.com>
2023-12-25 16:31:02 +0900

    수동테스트 VS 자동화된 테스트

---
+++ src/main/java/sample/cafekiosk/unit/CafeKiosk.java
@@ -0,0 +1,37 @@
+package sample.cafekiosk.unit;
+
+import lombok.Getter;
+import sample.cafekiosk.unit.beverage.Beverage;
+import sample.cafekiosk.unit.order.Order;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class CafeKiosk {
+
+    private final List<Beverage> beverages = new ArrayList<>();
+
+    public void add(Beverage beverage) {
+        beverages.add(beverage);
+    }
+
+    public void remove(Beverage beverage) {
+        beverages.remove(beverage);
+    }
+
+    public void clear() {
+        beverages.clear();
+    }
+
+    public int calculateTotalPrice() {
+        return beverages.stream()
+                .mapToInt(Beverage::getPrice)
+                .sum();
+    }
+
+    public Order createOrder() {
+        return new Order(LocalDateTime.now(), beverages);
+    }
+}
---
+++ src/main/java/sample/cafekiosk/unit/beverage/Beverage.java
@@ -0,0 +1,8 @@
+package sample.cafekiosk.unit.beverage;
+
+public interface Beverage {
+
+    String getName();
+
+    int getPrice();
+}
